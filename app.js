014a03ba21e51bcf7a17ab7cfb3739dd85f837a2
Tom <58078313+reed-tom@users.noreply.github.com>
2021-01-27 15:22:08 -0500

    added logging component and updated outdated packages

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ let app = express();
 var fs = require("fs");
 var http = require('http');
 var https = require('https');
-const proxy = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 const debug = require('debug');
+var cors = require('cors');
+var logger = require('./logger');
 
+
+app.options('*', cors());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -14,22 +18,33 @@ app.use(function(req, res, next) {
     // FORCE HTTPS
     if(!req.secure){
         res.redirect("https://" + req.headers.host + req.url);
+        logger.info(`HTTP -> HTTPS Redirect`);
     }
-
     next();
   });
 
 // Config
 const { routes } = require('./config.json');
-
-
-
 for (route of routes) {
     app.use(route.route,
-        proxy({
+        createProxyMiddleware({
             target: route.address,
+            onProxyReq: (proxyReq, req,res) => {
+                // SET THE ORIGINAL IP.  USED IN APPSTATS FOR NOW
+                var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+                ip = ip.replace("::ffff:", "");
+                logger.info(`${ip}`);
+                // logger.debug(`LOG TO DEBUG LOG`);
+                // logger.error(`LOG TO ERROR LOG`);
+
+                //const ip = req.connection.remoteAddress.replace("::ffff:", "");
+                proxyReq.setHeader('proxy-ip', ip);
+            },
             pathRewrite: (path, req) => {
+                //TEST
+                //console.log(path.split('/').slice(2).join('/'))
                 console.log(path.split('/').slice(2).join('/'))
+                logger.info(`${path}`);
                 return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
             }
         })
@@ -46,10 +61,12 @@ const options = {
 
 // HTTPS
 var server = https.createServer(options, app).listen(443, function () {
+    logger.info('HTTPS (SSL) Express server listening on port 443');
     debug('HTTPS (SSL) Express server listening on port 443');
 });
 
 // HTTP
 var server = http.createServer(options, app).listen(80, function () {
+    logger.info('HTTP Express server listening on port ' + server.address().port);
     debug('HTTP Express server listening on port ' + server.address().port);
 });
